d508b5dfdb3c4cfa759cdd0b9c089276cdeb9689
Pham Nguyen Khoa <khoapnse183214@fpt.edu.vn>
2024-11-29 17:32:48 +0700

    Cấu hình lại views( layouts, mixins)

--- controller/client/topic.controller.ts
+++ controller/client/topic.controller.ts
@@ -4,12 +4,13 @@ import { Request,Response } from "express"
 import Topics from "../../models/topics.model"
 
 
-export const index = async (req: Request, res:Response) => {
+export const topics = async (req: Request, res:Response) => {
     const listTopics = await Topics.find({
       deleted: false
     })
     console.log(listTopics)
     res.render("client/pages/topics/index.pug",{
+        pageTitle: "Trang chủ đề bài hát",
       listTopics
     })
   }
--- routers/client/topic.router.ts
+++ routers/client/topic.router.ts
@@ -6,7 +6,7 @@ import * as controller from "../../controller/client/topic.controller";
 
 const router: Router = Router();
 
-router.get ("/" ,controller.index);
+router.get ("/" ,controller.topics);
 
 
 export const topicRouter: Router =  router;
