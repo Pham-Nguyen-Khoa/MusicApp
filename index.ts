35f04ea67f1f50eadbb073426b2b148258a5cfda
Pham Nguyen Khoa <khoapnse183214@fpt.edu.vn>
2024-11-29 16:47:20 +0700

    Kết nối database

--- index.ts
+++ index.ts
@@ -1,15 +1,30 @@
 import express, {Express, Response, Request} from "express"
 import dotenv from "dotenv"
-
-const app:Express = express()
-const port:Number = 3000
+import * as database from "./config/database";
 
 //Cấu hình dotenv 
 dotenv.config();
 
 
+//Connect Database
+database.connect();
+
+
+const app:Express = express()
+const port: String | Number  = process.env.PORT || 3000;
+
+
+//Cấu hình views
+app.set("views","./views");
+app.set("view engine","pug");
+
+
+
+
+
+
 app.get('/topics', (req: Request, res:Response) => {
-  res.send('Danh sách các chủ đề ')
+  res.render("client/pages/topics/index.pug")
 })
 
 app.listen(port, () => {
